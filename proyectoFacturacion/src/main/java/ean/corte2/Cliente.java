996552f8299ea54facc2e7349f554aa27425a622
Johan H <jojhan_hernandez@hotmail.es>
2024-03-18 09:26:43 -0500

    Clase cliente creada

--- proyectoFacturacion/src/main/java/ean/corte2/Cliente.java
+++ proyectoFacturacion/src/main/java/ean/corte2/Cliente.java
@@ -1,7 +1,31 @@
 package ean.corte2;
 
 public class Cliente {
-    public Cliente(){ 
+    private int cedula;
+    private String nombre;
+    private int telefono;
+    public Cliente(int cedula, String nombre, int telefono){ 
         // cliente id -> Cedula
+        this.cedula = cedula;
+        this.nombre = nombre;
+        this.telefono = telefono;
+    }
+    public int getCedula() {
+        return cedula;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public int getTelefono() {
+        return telefono;
+    }
+    public void setCedula(int cedula) {
+        this.cedula = cedula;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
     }
 }
--- proyectoFacturacion/src/main/java/ean/corte2/Factura.java
+++ proyectoFacturacion/src/main/java/ean/corte2/Factura.java
@@ -3,12 +3,14 @@ import ean.corte2.*;
 
 public class Factura {
     InventarioProductos inventario;
+    Cliente cliente;
     Object[][] factura;
-    public Factura(InventarioProductos inventario){
+    public Factura(InventarioProductos inventario, Cliente cliente){
         this.inventario = inventario;
+        this.cliente = cliente;
     }
     public static void añadirAFactura(){
-
+        
     }
 
 }
